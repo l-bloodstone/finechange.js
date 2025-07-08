899a5491e8e948c7f20f0e564eeda0c66a7396da
Lucas Bloodstone <lucas.bloodstone@outlook.com>
2025-07-08 20:54:32 +0600

    feat: can chain exchange method based on previous exchange
    fix: package.js

--- src/exchanger.js
+++ src/exchanger.js
@@ -11,6 +11,18 @@ export default class Exchanger {
         this.xamount = xamount
     }
 
+    __generateNewExchangeRate(baseCurrency, exchangeRates) {
+        const newExchangeRate = { }
+        for (const currency in exchangeRates) {
+            if (currency === baseCurrency) {
+                continue
+            }
+            newExchangeRate[currency] = exchangeRates[currency] / exchangeRates[baseCurrency]
+        }
+        newExchangeRate[baseCurrency] = 1
+        return newExchangeRate
+    }
+
     exchange(targetCurrency) {
         let targetCurrencyRate = 0
         if (this.exchangeRates.hasOwnProperty(targetCurrency)) {
@@ -20,6 +32,7 @@ export default class Exchanger {
             throw Error("[ERROR] Exchange rate can not be negative or zero")
         }
         const amount = parseInt(this.xamount.amount * targetCurrencyRate)
-        return new XAmount().fromCents(amount).initExchanger(this.exchangeRates)
+        return new XAmount().fromCents(amount)
+            .initExchanger(this.__generateNewExchangeRate(targetCurrency, this.exchangeRates))
     }
 }
